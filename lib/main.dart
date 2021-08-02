2c5a15d18c9d53fd051780578ab121f5a1de61a1
Saurav Sah <saurav.sah@mavs.uta.edu>
2021-08-01 21:14:12 -0500

    final commit

--- lib/main.dart
+++ lib/main.dart
@@ -12,35 +12,34 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: <Widget>[
-              Container(
+            child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            Container(
                 color: Colors.red,
                 height: double.infinity,
                 width: 100,
+                margin: const EdgeInsets.only(left: 0)),
+            SizedBox(width: 20),
+            Container(
+              color: Colors.blue,
+              height: 200,
+              width: 100,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Container(color: Colors.black, height: 100),
+                ],
               ),
-              SizedBox(width: 20),
-              Container(
-                color: Colors.blue,
-                height: 200,
-                width: 100,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    Container(color: Colors.black, height: 100),
-                  ],
-                ),
-              ),
-              SizedBox(width: 20),
-              Container(
-                color: Colors.white,
-                height: double.infinity,
-                width: 100,
-              ),
-            ],
-          ),
-        ),
+            ),
+            SizedBox(width: 20),
+            Container(
+              color: Colors.white,
+              height: double.infinity,
+              width: 100,
+            ),
+          ],
+        )),
       ),
     );
   }
